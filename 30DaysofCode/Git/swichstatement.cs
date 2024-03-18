f1280130ee42e02f28287d56c1c671b97f9bb1d0
Dharmendra9695 <dharmendrara969594@gmail.com>
2024-03-18 11:31:26 +0000

    Code addedd

---
+++ 30DaysofCode/Git/swichstatement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30DaysofCode.Git
+{
+    internal class swichstatement
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello swith statement");
+            Console.ReadLine();
+        }
+    }
+}
